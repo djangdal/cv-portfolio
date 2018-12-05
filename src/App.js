a893f81fc664a1a3c5086ff379f8836a33df735e
David Jangdal <djangdal@gmail.com>
2018-12-05 14:36:54 +0100

    added content and upload to firebase

--- src/App.js
+++ src/App.js
@@ -9,36 +9,45 @@ class App extends Component {
     const projects = [
     {
       title: 'Lead iOS Developer, Min Volvo iOS App', 
-      description: 'Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. ', 
+      description: 'Volvo is always striving to create the best customer experience. Thats why we developed an app that would handle everything regarding your car. From insurance to maintenance to controlling it. I was the lead developer in a team of 6 people', 
       image: 'min_volvo_image.png',
       logo: 'min_volvo_logo.png',
       link: 'https://itunes.apple.com/se/app/min-volvo/id1340848952?mt=8'
     },{
-      title: 'Lead iOS Developer,\nLego Star Wars Force Builder iOS App', 
-      description: 'Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. ', 
+      title: 'Lead iOS Developer, Goda Grannar iOS App', 
+      description: 'One of the top insurance companies, Länsförsäkringar, in Sweden wanted to increase the safety in neighbourhoods. The app handles communication across the residents in their neighbourhood. I was the lead developer for the iOS app', 
+      image: 'goda_grannar_image.png',
+      logo: null,
+    },{
+      title: 'Unity Developer,\nLego Star Wars Force Builder mobile Game', 
+      description: 'In conjunction with the new Star Wars movie, Lego wanted to realese a new game containing the new characters and ships from the movie. I was part of a 4 man dev team', 
       image: 'force_builder_image.png',
       logo: 'force_builder_logo.png',
       link: 'https://www.lego.com/en-us/themes/star-wars/articles/star-wars-force-builder-app-post-762cf80c2ff140b2b7e87b96f2b64551'
     },{
-      title: 'iOS Developer, Dplay iOS App', 
-      description: 'Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. ', 
-      image: 'dplay_image.png',
-      logo: 'dplay_logo.png',
-      link: 'https://itunes.apple.com/se/app/dplay/id395972146?mt=8'
-    },
-    {
-      title: 'Developer, Electrolux', 
-      description: 'Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. ', 
+      title: 'Lead Developer, Electrolux', 
+      description: 'To join the era of IoT, we created a library to handle the connection between appliances and the mobile phone. I was co-lead on the 5 man iOS team', 
       image: 'electrolux_image.png',
       logo: 'electrolux_logo.png',
       link: null
-    },
-    {
-      title: 'Lead iOS Developer, Bamseklubben iOS App', 
-      description: 'Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. ', 
+    },{
+      title: 'Founder and Project leader, Monpie Game.', 
+      description: 'Together with 5 other people I created a game for iOS that reached first place on App Store with more than 100,000 downloads in 24 hours. I was the founder as well as project leader and lead Developer', 
+      image: 'monpie_image.png',
+      logo: 'monpie_logo.png',
+      link: "https://itunes.apple.com/us/app/monpie-a-monster-pie-adventure/id705804178?mt=8"
+    },{
+      title: 'Lead Unity Developer, Bamseklubben mobile Game', 
+      description: 'A Swedish travel agency partnered up with a famous Character called Bamse, to make a better experience for the children. I was the Lead developer ', 
       image: 'bamse_image.png',
       logo: 'bamse_logo.png',
       link: 'https://itunes.apple.com/se/app/bamseklubben-tui-barnspel/id1235584567?mt=8'
+    },{
+      title: 'iOS Developer, Dplay iOS App', 
+      description: 'To make their content available to more people, Discovery created an online streaming service called Dplay available on both mobile and web. I was one of 5 iOS developers', 
+      image: 'dplay_image.png',
+      logo: 'dplay_logo.png',
+      link: 'https://itunes.apple.com/se/app/dplay/id395972146?mt=8'
     }];
 
     const rows = [];
@@ -66,8 +75,9 @@ class ProfileHeader extends React.Component {
         <img id="profile-image" src="david.png" alt="profile"/>
         <div id="profile-info">
           <h2 id="profile-title">Hej, I'm David, an iOS developer</h2>
-          <p id="profile-description">Lorem ipsum dolor amet mlkshk direct trade wayfarers lumbersexual banh mi sustainable distillery godard. Keffiyeh try-hard plaid vinyl asymmetrical shaman pinterest, af normcore kickstarter truffaut fashion axe hexagon trust fund.</p>
-          <a href="https://github.com/djangdal" rel="noopener noreferrer" target="_blank">GitHub</a>
+          <p id="profile-description">Grew up in the cold woods of nothern Sweden, in a town called Luleå. I learned how to shovel snow, handle an axe, drive a snowmobile and fix my car. During the winter we had 3 hours of sunlight, in a month, if lucky. Maybe that's why i developed my interest in computers, well, gaming. I only gained my coding skills from five years of Computer Science and a master in Machine Learning at the <a href="https://www.kth.se/en" rel="noopener noreferrer" target="_blank" id="kth-link"> Royal Institute of technology</a>. These days I'm building more games then I play along with specializing in swift for iOS development, and I love it.</p>
+          <a href="https://github.com/djangdal" rel="noopener noreferrer" target="_blank" class="profile-link">GitHub</a>
+          <a href="https://www.linkedin.com/in/david-jangdal-43359551/" rel="noopener noreferrer" target="_blank" class="profile-link">LinkedIn</a>
         </div>
       </div>
     );    
@@ -121,9 +131,9 @@ class ProjectInformation extends React.Component {
         <div className="project-info-left">
         <p className="project-info-title">{project.title}</p>
         <p className="project-info-description">{project.description}</p>
-        {project.link != null && <a href={project.link} rel="noopener noreferrer" target="_blank">visit page</a>}
+        {project.link != null && <a href={project.link} rel="noopener noreferrer" target="_blank" class="project-info-link">visit page</a>}
         <br/>
-        <img src={project.logo} alt={project.logo} className="project-info-logo"/>
+        {project.logo != null &&<img src={project.logo} alt={project.logo} className="project-info-logo"/>}
         </div>
       );
     } else {
@@ -131,9 +141,9 @@ class ProjectInformation extends React.Component {
         <div className="project-info-right">
         <p className="project-info-title">{project.title}</p>
         <p className="project-info-description">{project.description}</p>
-        {project.link != null && <a href={project.link} rel="noopener noreferrer" target="_blank">visit page</a>}
+        {project.link != null && <a href={project.link} rel="noopener noreferrer" target="_blank" class="project-info-link">visit page</a>}
         <br/>
-        <img src={project.logo} alt={project.logo} className="project-info-logo"/>
+        {project.logo != null &&<img src={project.logo} alt={project.logo} className="project-info-logo"/>}
         </div>
       );
     }
